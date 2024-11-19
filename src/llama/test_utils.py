e5c0d7c9fd142dcb449daae00b9bc7c4cc6e3e52
Hanna Ruth <hruth@pigeon.xray.aps.anl.gov>
2024-11-19 16:59:18 -0600

    fixed a type in the test_shift_class file

--- src/llama/test_utils.py
+++ src/llama/test_utils.py
@@ -52,8 +52,8 @@ def load_input_projection_data(filename: str) -> tuple[np.ndarray, np.ndarray]:
     return complex_projections, angles
 
 
-def compare_data(data: np.ndarray, test_name: str, variable_type: ResultType, atol=1e-3, rtol=1e-3):
-    filepath = generate_results_path(test_name, variable_type)
+def compare_data(data: np.ndarray, test_name: str, comparison_test_name: str, variable_type: ResultType, atol=1e-3, rtol=1e-3):
+    filepath = generate_results_path(comparison_test_name, variable_type)
     old_data = np.load(filepath)
     if not np.allclose(data, old_data, atol=atol, rtol=rtol):
         raise AssertionError
--- tests/unit tests/test_shift_class.py
+++ tests/unit tests/test_shift_class.py
@@ -44,7 +44,7 @@ def check_or_record_results(
     if overwrite_results:
         tutils.save_results_data(results, test_name, tutils.ResultType.PROJECTIONS_COMPLEX)
     else:
-        tutils.compare_data(results, comparison_test_name, tutils.ResultType.PROJECTIONS_COMPLEX)
+        tutils.compare_data(results, test_name, comparison_test_name, tutils.ResultType.PROJECTIONS_COMPLEX)
 
 
 def test_fft_shift_class_cpu(pytestconfig, overwrite_results=False, return_results=False):
@@ -60,7 +60,7 @@ def test_fft_shift_class_gpu(pytestconfig, overwrite_results=False, return_resul
         overwrite_results = pytestconfig.getoption("overwrite_results")
     test_name = "test_fft_shift_class_gpu"
     comparison_test_name = "test_fft_shift_class_cpu"
-    shifted_projections = prepare_data(enums.DeviceType.CPU, enums.ShiftType.FFT)
+    shifted_projections = prepare_data(enums.DeviceType.GPU, enums.ShiftType.FFT)
     check_or_record_results(shifted_projections, test_name, comparison_test_name, overwrite_results)
 
 
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     parser.add_argument("--overwrite-results", action="store_true")
     args = parser.parse_args()
 
-    # test_fft_shift_class_cpu(None, overwrite_results=args.overwrite_results)
-    # test_fft_shift_class_gpu(None, overwrite_results=args.overwrite_results)
+    test_fft_shift_class_cpu(None, overwrite_results=args.overwrite_results)
+    test_fft_shift_class_gpu(None, overwrite_results=args.overwrite_results)
     test_circ_shift_class_cpu(None, overwrite_results=args.overwrite_results)
     test_circ_shift_class_gpu(None, overwrite_results=args.overwrite_results)
