4c75ec22263edb1a5d73e3c3be6d059b82ce2e49
Hanna Ruth <hruth@pigeon.xray.aps.anl.gov>
2024-11-19 16:34:11 -0600

    updated shift testing to include circ shift

--- tests/unit tests/test_shift_class.py
+++ tests/unit tests/test_shift_class.py
@@ -13,18 +13,13 @@ import llama.test_utils as tutils
 filename = "cSAXS_projections_downsampling16.h5"
 
 
-def test_shift_class(pytestconfig, overwrite_results=False, return_results=False):
-    if pytestconfig is not None:
-        overwrite_results = pytestconfig.getoption("overwrite_results")
-
-    test_name = "test_shift_class"
-
+def prepare_data(
+    device_type: enums.DeviceType, shift_type: enums.ShiftType
+) -> tuple[ComplexProjections, np.ndarray]:
     complex_projections, angles = tutils.load_input_projection_data(filename)
 
     projection_options = ProjectionOptions()
-    complex_projections = ComplexProjections(
-        complex_projections, angles, projection_options
-    )
+    complex_projections = ComplexProjections(complex_projections, angles, projection_options)
 
     n_images = len(angles)
     shift_array_size = (complex_projections.n_projections, 2)
@@ -35,61 +30,55 @@ def test_shift_class(pytestconfig, overwrite_results=False, return_results=False
 
     shift_options = ShiftOptions()
     shift_options.enabled = True
+    shift_options.device_options.device_type = device_type
+    shift_options.type = shift_type
     shifter = Shifter(shift_options)
     shifted_projections = shifter.run(complex_projections.data, shift)
 
+    return shifted_projections
+
+
+def check_or_record_results(
+    results: np.ndarray, test_name: str, comparison_test_name: str, overwrite_results: bool
+):
     if overwrite_results:
-        tutils.save_results_data(
-            shifted_projections, test_name, tutils.ResultType.PROJECTIONS_COMPLEX
-        )
-    elif return_results:
-        # For viewing results in jupyter notebooks
-        return shifted_projections, complex_projections
+        tutils.save_results_data(results, test_name, tutils.ResultType.PROJECTIONS_COMPLEX)
     else:
-        tutils.compare_data(
-            shifted_projections, test_name, tutils.ResultType.PROJECTIONS_COMPLEX
-        )
+        tutils.compare_data(results, comparison_test_name, tutils.ResultType.PROJECTIONS_COMPLEX)
 
 
-def test_shift_class_gpu(pytestconfig, overwrite_results=False, return_results=False):
+def test_fft_shift_class_cpu(pytestconfig, overwrite_results=False, return_results=False):
     if pytestconfig is not None:
         overwrite_results = pytestconfig.getoption("overwrite_results")
-    test_name = "test_shift_class_gpu"
-    comparison_test_name = "test_shift_class"
+    test_name = "test_fft_shift_class_cpu"
+    shifted_projections = prepare_data(enums.DeviceType.CPU, enums.ShiftType.FFT)
+    check_or_record_results(shifted_projections, test_name, test_name, overwrite_results)
 
-    complex_projections, angles = tutils.load_input_projection_data(filename)
 
-    projection_options = ProjectionOptions()
-    complex_projections = ComplexProjections(
-        complex_projections, angles, projection_options
-    )
+def test_fft_shift_class_gpu(pytestconfig, overwrite_results=False, return_results=False):
+    if pytestconfig is not None:
+        overwrite_results = pytestconfig.getoption("overwrite_results")
+    test_name = "test_fft_shift_class_gpu"
+    comparison_test_name = "test_fft_shift_class_cpu"
+    shifted_projections = prepare_data(enums.DeviceType.CPU, enums.ShiftType.FFT)
+    check_or_record_results(shifted_projections, test_name, comparison_test_name, overwrite_results)
 
-    n_images = len(angles)
-    shift_array_size = (complex_projections.n_projections, 2)
-    shift = (
-        np.zeros(shift_array_size)
-        + np.linspace(0, complex_projections.data.shape[2], n_images)[:, None]
-    ).astype(np.float32)
 
-    shift_options = ShiftOptions()
-    shift_options.enabled = True
-    shift_options.device_options.device_type = enums.DeviceType.GPU
-    shifter = Shifter(shift_options)
-    shifted_projections = shifter.run(complex_projections.data, shift)
+def test_circ_shift_class_gpu(pytestconfig, overwrite_results=False, return_results=False):
+    if pytestconfig is not None:
+        overwrite_results = pytestconfig.getoption("overwrite_results")
+    test_name = "test_circ_shift_class_cpu"
+    shifted_projections = prepare_data(enums.DeviceType.CPU, enums.ShiftType.CIRC)
+    check_or_record_results(shifted_projections, test_name, test_name, overwrite_results)
 
-    if overwrite_results:
-        tutils.save_results_data(
-            shifted_projections, test_name, tutils.ResultType.PROJECTIONS_COMPLEX
-        )
-    if return_results:
-        # For viewing results in jupyter notebooks
-        return shifted_projections, complex_projections
-    else:
-        tutils.compare_data(
-            shifted_projections,
-            comparison_test_name,
-            tutils.ResultType.PROJECTIONS_COMPLEX,
-        )
+
+def test_circ_shift_class_cpu(pytestconfig, overwrite_results=False, return_results=False):
+    if pytestconfig is not None:
+        overwrite_results = pytestconfig.getoption("overwrite_results")
+    test_name = "test_circ_shift_class_gpu"
+    comparison_test_name = "test_circ_shift_class_cpu"
+    shifted_projections = prepare_data(enums.DeviceType.GPU, enums.ShiftType.CIRC)
+    check_or_record_results(shifted_projections, test_name, comparison_test_name, overwrite_results)
 
 
 if __name__ == "__main__":
@@ -97,5 +86,7 @@ if __name__ == "__main__":
     parser.add_argument("--overwrite-results", action="store_true")
     args = parser.parse_args()
 
-    # test_shift_class(None, overwrite_results=args.overwrite_results)
-    test_shift_class_gpu(None, overwrite_results=args.overwrite_results)
+    # test_fft_shift_class_cpu(None, overwrite_results=args.overwrite_results)
+    # test_fft_shift_class_gpu(None, overwrite_results=args.overwrite_results)
+    test_circ_shift_class_cpu(None, overwrite_results=args.overwrite_results)
+    test_circ_shift_class_gpu(None, overwrite_results=args.overwrite_results)
