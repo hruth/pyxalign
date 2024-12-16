Fixed a directional error with CoR Offset in calculation of astra vectors

src/llama/alignment/projection_matching.py
@@ -36,6 +36,7 @@ class ProjectionMatchingAligner(Aligner):
     def run(self) -> np.ndarray:
         # Create the projections object
         projection_options = copy.deepcopy(self.projections.options)
+        projection_options.experiment.pixel_size = self.projections.pixel_size
         projection_options.crop = self.options.crop
         projection_options.downsample = self.options.downsample
         projection_options.reconstruct = self.options.reconstruct
@@ -59,7 +60,7 @@ class ProjectionMatchingAligner(Aligner):
         # Run the PMA algorithm
         self.calculate_alignment_shift()
 
-        if self.memory_config is MemoryConfig.GPU_ONLY:
+        if self.memory_config == MemoryConfig.GPU_ONLY:
             self.move_arrays_back_to_cpu()
 
         # Re-scale the shift
@@ -132,7 +133,7 @@ class ProjectionMatchingAligner(Aligner):
         self.n_pix = self.aligned_projections.reconstructed_object_dimensions
         self.mass = xp.median(xp.abs(self.aligned_projections.data).mean(axis=(1, 2)))
         # Prepare pre-allocated and pinned arrays
-        if self.memory_config is not MemoryConfig.CPU_ONLY:
+        if not (self.memory_config == MemoryConfig.CPU_ONLY):
             self.pinned_filtered_sinogram = gutils.pin_memory(
                 np.empty(self.aligned_projections.data.shape, dtype=r_type)
             )
@@ -140,11 +141,11 @@ class ProjectionMatchingAligner(Aligner):
                 np.empty(self.aligned_projections.data.shape, dtype=r_type)
             )
             n_proj = self.aligned_projections.n_projections
-            if self.memory_config is MemoryConfig.MIXED:
+            if self.memory_config == MemoryConfig.MIXED:
                 self.shift_update = gutils.pin_memory(np.empty((n_proj, 2), dtype=r_type))
                 self.pinned_error = gutils.pin_memory(np.empty((n_proj), dtype=r_type))
                 self.pinned_unfiltered_error = gutils.pin_memory(np.empty((n_proj), dtype=r_type))
-            elif self.memory_config is MemoryConfig.GPU_ONLY:
+            elif self.memory_config == MemoryConfig.GPU_ONLY:
                 self.shift_update = cp.empty((n_proj, 2), dtype=r_type) # type: ignore
                 self.pinned_error = cp.empty((n_proj), dtype=r_type) # type: ignore
                 self.pinned_unfiltered_error = cp.empty((n_proj), dtype=r_type) # type: ignore
@@ -172,7 +173,7 @@ class ProjectionMatchingAligner(Aligner):
         # to move arrays to the gpu in chunks if they 
         # are on the cpu. For now, just move the whole
         # array.
-        if self.memory_config is MemoryConfig.GPU_ONLY:
+        if self.memory_config == MemoryConfig.GPU_ONLY:
             forward_projection_input = cp.array(
                 self.aligned_projections.laminogram.forward_projections.data
             )
@@ -283,16 +284,16 @@ class ProjectionMatchingAligner(Aligner):
             self.options.keep_on_gpu, self.options.device.device_type
         )
 
-        if self.memory_config is MemoryConfig.GPU_ONLY:
+        if self.memory_config == MemoryConfig.GPU_ONLY:
             cp.cuda.Device(self.options.device.gpu.gpu_indices[0]).use()
             initializer_function = cp.array
             self.xp = cp
             self.scipy_module = gutils.get_scipy_module(cp.array(1))
-        elif self.memory_config is MemoryConfig.MIXED:
+        elif self.memory_config == MemoryConfig.MIXED:
             initializer_function = gutils.pin_memory
             self.xp = np
             self.scipy_module = gutils.get_scipy_module(cp.array(1))
-        elif self.memory_config is MemoryConfig.CPU_ONLY:
+        elif self.memory_config == MemoryConfig.CPU_ONLY:
             initializer_function = lambda x: (x * 1)  # noqa: E731
             self.xp = np
             self.scipy_module = gutils.get_scipy_module(np.array(1))
@@ -300,7 +301,7 @@ class ProjectionMatchingAligner(Aligner):
         unshifted_projections = initializer_function(self.aligned_projections.data)
         unshifted_masks = initializer_function(self.aligned_projections.masks)
 
-        if self.memory_config is not MemoryConfig.CPU_ONLY:
+        if not (self.memory_config == MemoryConfig.CPU_ONLY):
             self.aligned_projections.data = initializer_function(self.aligned_projections.data)
             self.aligned_projections.masks = initializer_function(self.aligned_projections.masks)
 
@@ -312,7 +313,7 @@ class ProjectionMatchingAligner(Aligner):
 
     def initialize_shifters(self):
         device_options = copy.deepcopy(self.options.device)
-        if self.memory_config is MemoryConfig.GPU_ONLY:
+        if self.memory_config == MemoryConfig.GPU_ONLY:
             device_options.device_type = DeviceType.GPU
 
         projections_shift_options = ShiftOptions(
@@ -335,7 +336,7 @@ class ProjectionMatchingAligner(Aligner):
         # Generate circular mask for reconstruction
         circulo = self.aligned_projections.laminogram.get_circular_window()
 
-        if self.memory_config is MemoryConfig.MIXED:
+        if self.memory_config == MemoryConfig.MIXED:
             tukey_window = gutils.pin_memory(tukey_window)
 
         return tukey_window, circulo

src/llama/dev_utils.py
@@ -1,13 +1,22 @@
+from dataclasses import dataclass
 import importlib
 import sys
 import inspect
+from typing import List
 
-def reload_module_and_imports(module_name, namespace=None):
+from llama.projections import ComplexProjections, PhaseProjections, Projections
+from llama.api.options import ProjectionOptions
+
+# To do: prevent overwriting! everything is imported by name which is bad!!
+# Just switch to lamReload model (with submodules being reloaded) and manually re-do imports
+
+def reload_module_and_imports(module_name="llama", namespace=None):
     """
     Reloads a module, its submodules, and dynamically re-imports all attributes
     into the specified namespace.
 
-    The call must be reload_module_and_imports("llama", globals()) for this to work.
+    The call must be `reload_module_and_imports("llama", globals())` for this to work.
+    Note that using this breaks `isinstance` when objects get reloaded.
 
     Args:
         module_name (str): The top-level module name to reload.
@@ -15,14 +24,14 @@ def reload_module_and_imports(module_name, namespace=None):
     """
     if module_name not in sys.modules:
         raise ImportError(f"Module '{module_name}' is not currently loaded.")
-    
+
     # Default to the global namespace of the caller
     if namespace is None:
         namespace = globals()
 
     # Collect all submodules
     modules_to_reload = [name for name in sys.modules if name.startswith(module_name)]
-    
+
     # Some things aren't updated on the first pass, so I reload way too many
     # times to ensure everything is reloaded properly.
     num_reloads = 10
@@ -30,10 +39,52 @@ def reload_module_and_imports(module_name, namespace=None):
         # Reload all modules in reverse order (to handle dependencies correctly)
         for name in sorted(modules_to_reload, key=len, reverse=True):
             importlib.reload(sys.modules[name])
-        
+
         # Re-import all attributes from submodules into the specified namespace
         for name in modules_to_reload:
             module = sys.modules[name]
+            # if i == 0:
+                # print(module)
             for attr_name, attr_value in inspect.getmembers(module):
                 if not attr_name.startswith("_"):  # Skip private attributes
+                    print(attr_name)
                     namespace[attr_name] = attr_value
+
+
+def refresh_projections(stale_projections: Projections) -> Projections:
+    for projection_class in [PhaseProjections, ComplexProjections]:
+        if projection_class.__name__ == stale_projections.__class__.__name__:
+            # PhaseProjections()
+            new_projections = projection_class(
+                projections=stale_projections.data,
+                angles=stale_projections.angles,
+                options=refresh_options(stale_projections.options, ProjectionOptions),
+                masks=stale_projections.masks,
+                shift_manager=stale_projections.shift_manager,
+                center_of_rotation=stale_projections.center_of_rotation,
+                skip_pre_processing=True,
+            )
+            new_projections.pixel_size = stale_projections.pixel_size
+    return new_projections
+
+
+def refresh_options(stale_options: dataclass, options_class: dataclass) -> dataclass:
+    # This won't work if fields were removed.
+    return options_class(**vars(stale_options))
+
+
+if __name__ == "__main__":
+    from llama.api.options import DownsampleOptions
+    import numpy as np
+
+    p = PhaseProjections(
+        np.zeros((3, 3, 3)),
+        [1, 2, 3],
+        ProjectionOptions(downsample=DownsampleOptions(scale=20)),
+    )
+    print("p is not stale:", isinstance(p, PhaseProjections))
+    reload_module_and_imports("llama", globals())
+    print("p is not stale:", isinstance(p, PhaseProjections))
+    new_p = refresh_projections(p)
+    print("p is not stale:", isinstance(new_p, PhaseProjections))
+    print("options are not stale:", isinstance(new_p.options, ProjectionOptions))

src/llama/reconstruct.py
@@ -45,9 +45,7 @@ def get_astra_reconstructor_geometry(
     skew_angle = np.pi / 180 * skew_angle * np.ones((len(angles), 1))
     pixel_scale = pixel_scale * np.ones((len(angles), 2))
     rotation_center = np.array([center_of_rotation[1], center_of_rotation[0]], dtype=r_type)
-    CoR_offset = (
-        rotation_center - np.array(sinogram.shape[1:][::-1]) / 2
-    )  # Might need the sign flipped!
+    CoR_offset = (rotation_center - np.array(sinogram.shape[1:][::-1]) / 2)[::-1]
 
     # We generate the same geometry as the circular one above.
     vectors = np.zeros((len(angles), 12))