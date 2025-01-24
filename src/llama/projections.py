FIX: fixed bugs in rotation and shear functions, updated rotation and shear options, re-factored and fixed some issues with the initial processing done in Projections.__init__, added individual methods for handling projection transformations.

src/llama/projections.py
@@ -1,26 +1,63 @@
 from typing import List, Optional
 import numpy as np
+import cupyx
 import copy
+from llama.api.options import transform
 from llama.estimate_center import estimate_center_of_rotation, CenterOfRotationEstimateResults
 from llama.api import enums
 from llama.api.options.alignment import AlignmentOptions
 from llama.api.options.device import DeviceOptions
 
-from llama.api.options.projections import ProjectionOptions
-from llama.api.options.transform import ShiftOptions, UpsampleOptions
+from llama.api.options.projections import ProjectionOptions, ProjectionTransformOptions
+from llama.api.options.transform import (
+    CropOptions,
+    RotationOptions,
+    ShearOptions,
+    ShiftOptions,
+    UpsampleOptions,
+    DownsampleOptions,
+)
 import llama.gpu_utils as gpu_utils
 from llama.gpu_wrapper import device_handling_wrapper
 from llama.laminogram import Laminogram
 from llama.mask import estimate_reliability_region_mask, blur_masks
 
 import llama.plotting.plotters as plotters
-from llama.transformations.classes import Downsampler, Rotator, Shifter, Upsampler, Cropper
-from llama.transformations.functions import image_shift_fft
+from llama.timer import timer
+from llama.transformations.classes import Downsampler, Rotator, Shearer, Shifter, Upsampler, Cropper
+from llama.transformations.functions import image_shift_fft, will_rotation_flip_aspect_ratio
 from llama.unwrap import unwrap_phase
 from llama.api.types import ArrayType, r_type
 
 
+class TransformTracker:
+    def __init__(
+        self,
+        rotation: r_type = 0,
+        shear: r_type = 0,
+        crop=None,
+        downsample: int = 1,
+    ):
+        self.rotation = rotation
+        self.shear = shear
+        self.crop = crop
+        self.downsample = downsample
+
+    def update_rotation(self, angle: r_type):
+        self.rotation += angle
+
+    def update_shear(self, angle: r_type):
+        self.shear += angle
+
+    def update_crop(self, crop_options: CropOptions):
+        pass
+
+    def update_downsample(self, downsample: int):
+        self.downsample *= downsample
+
+
 class Projections:
+    @timer()
     def __init__(
         self,
         projections: np.ndarray,
@@ -30,37 +67,52 @@ class Projections:
         shift_manager: Optional["ShiftManager"] = None,
         center_of_rotation: Optional[np.ndarray] = None,
         skip_pre_processing: bool = False,
+        transform_tracker: Optional[TransformTracker] = None,
     ):
         self.options = options
         self.angles = angles
         self.data = projections
         self.masks = masks
+        self.pixel_size = self.options.experiment.pixel_size * 1
+        if transform_tracker is None:
+            self.transform_tracker = TransformTracker()
+        else:
+            self.transform_tracker = transform_tracker
         if center_of_rotation is None:
             self.center_of_rotation = np.array(self.data.shape[1:], dtype=r_type) / 2
         else:
             self.center_of_rotation = np.array(center_of_rotation, dtype=r_type)
 
         if not skip_pre_processing:
-            # Crop and downsample input data if enabled
-            self.data = Cropper(self.options.crop).run(self.data)
-            self.data = Downsampler(self.options.downsample).run(self.data)
-            if self.masks is not None:
-                self.masks = Cropper(self.options.crop).run(self.masks)
-                mask_downsample_options = copy.deepcopy(self.options.downsample)
-                mask_downsample_options.type = self.options.mask_downsample_type
-                mask_downsample_options.use_gaussian_filter = (
-                    self.options.mask_downsample_use_gaussian_filter
-                )
-                self.masks = Downsampler(mask_downsample_options).run(self.masks)
-            self.update_center_of_rotation()
+            self.transform_projections(self.options.input_processing)
+            # self.transform_projections(
+            #     self.options.crop,
+            #     self.options.downsample,
+            #     -self.options.experiment.tilt_angle,
+            #     -self.options.experiment.skew_angle,
+            #     self.options.mask_downsample_type,
+            #     self.options.mask_downsample_use_gaussian_filter
+            # )
+            # # Crop and downsample input data if enabled
+            # self.data = Cropper(self.options.crop).run(self.data)
+            # self.data = Downsampler(self.options.downsample).run(self.data)
+            # if self.masks is not None:
+            #     self.masks = Cropper(self.options.crop).run(self.masks)
+            #     mask_downsample_options = copy.deepcopy(self.options.downsample)
+            #     mask_downsample_options.type = self.options.mask_downsample_type
+            #     mask_downsample_options.use_gaussian_filter = (
+            #         self.options.mask_downsample_use_gaussian_filter
+            #     )
+            #     self.masks = Downsampler(mask_downsample_options).run(self.masks)
+            # self.update_center_of_rotation()
 
         # Update pixel size
         # When copying options, to a new projection, make sure to properly
         # set the pixel size!
-        if self.options.downsample.enabled and not skip_pre_processing:
-            self.pixel_size = self.options.experiment.pixel_size * self.options.downsample.scale
-        else:
-            self.pixel_size = self.options.experiment.pixel_size * 1
+        # if self.options.downsample.enabled and not skip_pre_processing:
+        #     self.pixel_size = self.options.experiment.pixel_size * self.options.downsample.scale
+        # else:
+        #     self.pixel_size = self.options.experiment.pixel_size * 1
 
         if shift_manager is not None:
             self.shift_manager = copy.deepcopy(shift_manager)
@@ -68,6 +120,100 @@ class Projections:
             self.shift_manager = ShiftManager(self.n_projections)
 
         self._post_init()
+
+    @timer()
+    def transform_projections(
+        self,
+        options: ProjectionTransformOptions,
+    ):
+        # # Transform the projections in the proper order
+        # if options.crop is not None:
+        #     self.crop_projections(options.crop)
+        # if options.downsample is not None:
+        #     self.downsample_projections(
+        #         options.downsample,
+        #         options.mask_downsample_type,
+        #         options.mask_downsample_use_gaussian_filter,
+        #     )
+        if options.rotation is not None:
+            self.rotate_projections(options.rotation)
+        if options.shear is not None:
+            self.shear_projections(options.shear)
+
+    def crop_projections(self, options: CropOptions):
+        if options.enabled:
+            self.data = Cropper(options).run(self.data)
+            if self.masks is not None:
+                self.masks = Cropper(options).run(self.masks)
+            self.transform_tracker.update_crop(options)
+            # To do: insert code for updating center of rotation
+
+    def downsample_projections(
+        self,
+        options: DownsampleOptions,
+        mask_downsample_type: enums.DownsampleType,
+        mask_downsample_use_gaussian_filter: bool,
+    ):
+        if options.enabled:
+            self.data = Downsampler(options).run(self.data)
+            if self.masks is not None:
+                mask_options = copy.deepcopy(options)
+                mask_options.type = mask_downsample_type
+                mask_options.use_gaussian_filter = mask_downsample_use_gaussian_filter
+                self.masks = Downsampler(mask_options).run(self.masks)
+            self.transform_tracker.update_downsample(options.scale)
+            # Update center of rotation
+            self.center_of_rotation = self.center_of_rotation / options.scale
+            # Update pixel size
+            self.pixel_size = self.pixel_size * options.scale
+
+    @timer()
+    def rotate_projections(self, options: RotationOptions):
+        # Note: the fourier rotation that is used for masks will likely
+        # be an issue. A new method needs to be implemented.
+        if options.enabled:
+            data_aspect_ratio_changes = will_rotation_flip_aspect_ratio(options.angle)
+            if not data_aspect_ratio_changes:
+                Rotator(options).run(self.data, pinned_results=self.data)
+                if self.masks is not None:
+                    Rotator(options).run(self.masks, pinned_results=self.masks)
+            else:
+                # If projections are already pinned, create a new pinned array
+                # at the new aspect ratio
+                if gpu_utils.is_pinned(self.data):
+                    # Note: for very large arrays where pinned memory
+                    # needs to be carefully managed, this may cause
+                    # problems since you are temporarily doubling the
+                    # amount of pinned memory.
+                    pinned_array = gpu_utils.create_empty_pinned_array(
+                        shape=(self.n_projections, *self.size[::-1]),
+                        dtype=self.data.dtype,
+                    )
+                    self.data = Rotator(options).run(self.data, pinned_results=pinned_array)
+                else:
+                    self.data = Rotator(options).run(self.data)
+                # Do the same procedure for the masks
+                if self.masks is not None:
+                    if gpu_utils.is_pinned(self.masks):
+                        pinned_array = gpu_utils.create_empty_pinned_array(
+                            shape=(self.n_projections, *self.size[::-1]),
+                            dtype=self.masks.dtype,
+                        )
+                        self.masks = Rotator(options).run(self.masks, pinned_results=pinned_array)
+                    else:
+                        self.masks = Rotator(options).run(self.masks)
+            self.transform_tracker.update_rotation(options.angle)
+            # To do: insert code for updating center of rotation
+
+    @timer()
+    def shear_projections(self, options: ShearOptions):
+        if options.enabled:
+            Shearer(options).run(self.data, pinned_results=self.data)
+            if self.masks is not None:
+                # Will probably be wrong without fixes
+                self.masks = Shearer(options).run(self.masks)
+            self.transform_tracker.update_shear(options.angle)
+            # To do: insert code for updating center of rotation
 
     @property
     def n_projections(self) -> int:
@@ -92,13 +238,13 @@ class Projections:
         "For running children specific code after instantiation"
         pass
 
-    def update_center_of_rotation(self):
-        if self.options.downsample.enabled:
-            self.center_of_rotation = self.center_of_rotation / self.options.downsample.scale
-        if self.options.crop.enabled:
-            # to do: add CoR update for when cropping is not symmetric. Will affect
-            # how to handle downsampling CoR as well.
-            pass
+    # def update_center_of_rotation(self):
+    #     if self.options.downsample.enabled:
+    #         self.center_of_rotation = self.center_of_rotation / self.options.downsample.scale
+    #     if self.options.crop.enabled:
+    #         # to do: add CoR update for when cropping is not symmetric. Will affect
+    #         # how to handle downsampling CoR as well.
+    #         pass
 
     def pin_projections(self):
         self.data = gpu_utils.pin_memory(self.data)
@@ -140,6 +286,7 @@ class Projections:
             enable_plotting=enable_plotting,
         )
         # Upsample results
+        # Keep on CPU for now -- can add upsampling device options later if needed
         upsample_options = UpsampleOptions(
             scale=downsample_options.scale, enabled=downsample_options.enabled
         )

src/llama/transformations/classes.py
@@ -41,7 +41,7 @@ class Downsampler(Transformation):
         super().__init__(options)
         self.options: DownsampleOptions = options
 
-    @timer()#"Downsampler")
+    @timer()
     def run(
         self,
         images: ArrayType,
@@ -71,6 +71,7 @@ class Downsampler(Transformation):
                     options=self.options.device,
                     chunkable_inputs_for_gpu_idx=[0],
                     pinned_results=pinned_results,
+                    display_progress_bar=True,
                 )
                 return self.function(
                     images,
@@ -89,7 +90,7 @@ class Upsampler(Transformation):
         super().__init__(options)
         self.options: UpsampleOptions = options
 
-    @timer()#"Upsampler")
+    @timer()
     def run(self, images: ArrayType, pinned_results: Optional[np.ndarray] = None) -> ArrayType:
         """Calls one of the image upsampling functions"""
         if self.enabled:
@@ -98,6 +99,7 @@ class Upsampler(Transformation):
                 options=self.options.device,
                 chunkable_inputs_for_gpu_idx=[0],
                 pinned_results=pinned_results,
+                display_progress_bar=True,
             )
             return self.function(images, self.options.scale)
         else:
@@ -112,7 +114,7 @@ class Shifter(Transformation):
         super().__init__(options)
         self.options: ShiftOptions = options
     
-    @timer()#"Shifter")
+    @timer()
     def run(
         self, images: ArrayType, shift: np.ndarray, pinned_results: Optional[np.ndarray] = None
     ) -> ArrayType:
@@ -137,9 +139,9 @@ class Rotator(Transformation):
         super().__init__(options)
         self.options: RotationOptions = options
 
-    @timer()#"Rotator")
+    @timer()
     def run(
-        self, images: ArrayType, angle: float, pinned_results: Optional[np.ndarray] = None
+        self, images: ArrayType, pinned_results: Optional[np.ndarray] = None
     ) -> ArrayType:
         """Calls one of the image rotation functions"""
         if self.enabled:
@@ -148,8 +150,9 @@ class Rotator(Transformation):
                 options=self.options.device,
                 chunkable_inputs_for_gpu_idx=[0],
                 pinned_results=pinned_results,
+                display_progress_bar=True,
             )
-            return self.function(images, angle)
+            return self.function(images, self.options.angle)
         else:
             return images
 
@@ -162,9 +165,9 @@ class Shearer(Transformation):
         super().__init__(options)
         self.options: ShearOptions = options
 
-    @timer()#"Shearer")
+    @timer()
     def run(
-        self, images: ArrayType, angle: float, pinned_results: Optional[np.ndarray] = None
+        self, images: ArrayType, pinned_results: Optional[np.ndarray] = None
     ) -> ArrayType:
         """Calls one of the image shearing functions"""
         if self.enabled:
@@ -173,8 +176,9 @@ class Shearer(Transformation):
                 options=self.options.device,
                 chunkable_inputs_for_gpu_idx=[0],
                 pinned_results=pinned_results,
+                display_progress_bar=True,
             )
-            return self.function(images, angle)
+            return self.function(images, self.options.angle)
         else:
             return images
 

src/llama/transformations/functions.py
@@ -306,25 +306,50 @@ def image_upsample_nearest(images: ArrayType, scale: int) -> ArrayType:
     return images.repeat(scale, axis=1).repeat(scale, axis=2)
 
 
+def will_rotation_flip_aspect_ratio(theta: float) -> bool:
+    n_rotations = round(theta / 90)
+    is_even = n_rotations % 2 == 0
+    return not is_even
+
+
+def rotateStackMod90(img, theta) -> ArrayType:
+    xp = cp.get_array_module(img)
+
+    numRotations = round(theta / 90)
+    img = xp.rot90(img, numRotations, axes=(1, 2))
+    theta = theta - 90 * numRotations
+
+    return img
+
+
 @timer()
 @preserve_complexity_or_realness()
-def image_rotate_fft(images: ArrayType, theta: float) -> ArrayType:
+def image_rotate_fft(
+    images: ArrayType, theta: float, preserve_aspect_ratio: bool = False
+) -> ArrayType:
     """Rotates the image around the z-axis (0th axis) of the input images"""
     xp = cp.get_array_module(images)
     scipy_module = get_scipy_module(images)
 
+    if not preserve_aspect_ratio:
+        # imageStack = lam.utils.rotateStackMod90(imageStack, tiltAngle)
+        images = rotateStackMod90(images, theta)
+        n_rotations = round(theta / 90)
+        theta = theta - 90 * n_rotations
+
     M, N = images.shape[1:]
 
-    n_rotations = round(theta / 90)
-    theta = theta - 90 * n_rotations
-
-    x_grid = xp.matrix(xp.arange(-np.fix(M / 2), np.ceil(M / 2), dtype=r_type)).transpose()
+    # x_grid = xp.matrix(xp.arange(-np.fix(M / 2), np.ceil(M / 2), dtype=r_type)).transpose()
+    x_grid = xp.arange(-np.fix(M / 2), np.ceil(M / 2), dtype=r_type)[:, None]
     x_grid = scipy_module.fft.ifftshift(x_grid) / M
-    y_grid = xp.matrix(xp.arange(-np.fix(N / 2), np.ceil(N / 2), dtype=r_type))
+    # y_grid = xp.matrix(xp.arange(-np.fix(N / 2), np.ceil(N / 2), dtype=r_type))
+    y_grid = xp.arange(-np.fix(N / 2), np.ceil(N / 2), dtype=r_type)[None, :]
     y_grid = scipy_module.fft.ifftshift(y_grid) / N
 
-    m_grid = xp.matrix(xp.arange(1, M + 1, dtype=r_type)).transpose() - xp.floor(M / 2) - 0.5
-    n_grid = xp.matrix(xp.arange(1, N + 1, dtype=r_type)) - xp.floor(N / 2) - 0.5
+    # m_grid = xp.matrix(xp.arange(1, M + 1, dtype=r_type)).transpose() - xp.floor(M / 2) - 0.5
+    # n_grid = xp.matrix(xp.arange(1, N + 1, dtype=r_type)) - xp.floor(N / 2) - 0.5
+    m_grid = xp.arange(1, M + 1, dtype=r_type)[:, None] - xp.floor(M / 2) - 0.5
+    n_grid = xp.arange(1, N + 1, dtype=r_type)[None, :] - xp.floor(N / 2) - 0.5
 
     n_x = -xp.sin(theta * xp.pi / 180) * x_grid
     n_y = xp.tan(theta / 2 * xp.pi / 180) * y_grid
@@ -348,12 +373,14 @@ def image_shear_fft(images: ArrayType, theta: float) -> ArrayType:
 
     M, N = images.shape[1:]
 
-    y_grid = xp.matrix(xp.arange(-np.fix(N / 2), np.ceil(N / 2), dtype=r_type))
+    # y_grid = xp.matrix(xp.arange(-np.fix(N / 2), np.ceil(N / 2), dtype=r_type))
+    y_grid = xp.arange(-np.fix(N / 2), np.ceil(N / 2), dtype=r_type)[None, :]
     y_grid = scipy_module.fft.ifftshift(y_grid) / N
-    n_y = xp.tan(theta / 2 * xp.pi / 180) * y_grid
-    m_grid = xp.array(
-        (xp.matrix(xp.arange(1, M + 1) - np.floor(M / 2)).transpose() * 2j * xp.pi), dtype=c_type
-    )
+    n_y = xp.tan(theta / 2 * xp.pi / 180).astype(xp.float32) * y_grid
+    # m_grid = xp.array(
+    #     (xp.matrix(xp.arange(1, M + 1) - np.floor(M / 2)).transpose() * 2j * xp.pi), dtype=c_type
+    # )
+    m_grid = xp.array(((xp.arange(1, M + 1) - np.floor(M / 2))[:, None] * 2j * xp.pi), dtype=c_type)
     images = scipy_module.fft.ifft(
         xp.multiply(scipy_module.fft.fft(images, axis=2), xp.exp(-xp.multiply(m_grid, n_y))), axis=2
     )