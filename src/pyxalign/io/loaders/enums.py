f7d565549c6ca033bb17dcc6549fbca999960748
Hanna Ruth <hruth@anl.gov>
2025-06-24 13:18:10 -0500

    Working on adding the button/functions for loading data from the gui

--- src/pyxalign/io/loaders/enums.py
+++ src/pyxalign/io/loaders/enums.py
@@ -11,3 +11,9 @@ class ExperimentInfoSourceType(StrEnum):
     LAMNI_DAT_FILE = auto()
     PTYCHO_FOLDERS = auto()
     BEAMLINE_2IDE_MDA_FILE = auto()
+
+
+class ExperimentType(StrEnum):
+    LYNX = auto()
+    BEAMLINE_2IDE_PTYCHO = auto()
+    BEAMLINE_2IDE_XRF = auto()
