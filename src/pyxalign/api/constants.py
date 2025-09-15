2ac118ba75a4676c6cbf4643522a2a1bbdb07591
Hanna Ruth <ruthanna@umich.edu>
2025-09-15 17:10:48 -0500

    FEAT: updated pyxalign.api.constants.divisor so that the user can edit it by setting environment variable PYXALIGN_DIVISOR before importing pyxalign

--- src/pyxalign/api/constants.py
+++ src/pyxalign/api/constants.py
@@ -1,1 +1,11 @@
-divisor = 32
+import os
+
+def is_power_of_two(n: int) -> bool:
+    return n > 0 and (n & (n - 1)) == 0
+
+default_divisor = 32
+new_divisor = int(os.environ.get("PYXALIGN_DIVISOR", default_divisor))
+if not is_power_of_two(new_divisor):
+    raise ValueError(f"The divisor must be a power of 2, but PYXALIGN_DIVISOR was set to {new_divisor}")
+else:
+    divisor = new_divisor
