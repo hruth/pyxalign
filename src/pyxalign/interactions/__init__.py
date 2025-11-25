fc03b103a91260dec58a178fe79714989eba1a18
Hanna Ruth <ruthanna@umich.edu>
2025-11-25 12:12:59 -0600

    removed ipython run_line_magic

--- src/pyxalign/interactions/__init__.py
+++ src/pyxalign/interactions/__init__.py
@@ -1,10 +1,10 @@
-from IPython import get_ipython
+# from IPython import get_ipython
 import pyqtgraph as pg
 
 pg.setConfigOption("background", "#242729")
 
 
-ipython = get_ipython()
-if ipython:
-    # ipython.magic("gui qt")
-    ipython.run_line_magic("gui", "qt")
+# ipython = get_ipython()
+# if ipython:
+#     # ipython.magic("gui qt")
+#     ipython.run_line_magic("gui", "qt")
