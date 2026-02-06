3d09b36c377e4bc493b5129be8499697b8825b4d
Hanna Ruth <hruth@anl.gov>
2026-02-06 14:53:34 -0600

    FEAT: added new method to SequencerWidget to allow auto-generation of a sequence from a list of dictionaries.

--- src/pyxalign/interactions/sequencer.py
+++ src/pyxalign/interactions/sequencer.py
@@ -20,7 +20,7 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import Qt
 from pyxalign.api.options.alignment import ProjectionMatchingOptions
-from pyxalign.api.types import OptionsClass
+from pyxalign.api.options.base import BaseOptions
 from pyxalign.interactions.options.options_editor import set_option_from_field_path
 
 from pyxalign.interactions.sequencer_item import SequencerItem
@@ -29,7 +29,7 @@ from pyxalign.interactions.utils.misc import switch_to_matplotlib_qt_backend
 
 class SequencerWidget(QWidget):
     def __init__(
-        self, options: OptionsClass, basic_options_list: Optional[list[str]] = None, parent=None
+        self, options: BaseOptions, basic_options_list: Optional[list[str]] = None, parent=None
     ):
         super().__init__(parent)
         self.options = options
@@ -74,8 +74,13 @@ class SequencerWidget(QWidget):
         button_layout.addWidget(self.remove_sequencer_button)
         self.main_layout.addLayout(button_layout)
 
-    def add_new_sequencer(self):
-        new_item = SequencerItem(self.options, basic_options_list=self.basic_options_list)
+    def add_new_sequencer(self, initial_state: Optional[tuple] = None):
+        # new_item = SequencerItem(self.options, basic_options_list=self.basic_options_list)
+        new_item = SequencerItem(
+            self.options,
+            initial_state=initial_state,
+            basic_options_list=self.basic_options_list,
+        )
         self.sequencer_items += [new_item]
         self.sequencer_list_layout.insertWidget(len(self.sequencer_items) - 1, new_item)
 
@@ -91,13 +96,34 @@ class SequencerWidget(QWidget):
         self.sequencer_items += [new_item]
         self.sequencer_list_layout.insertWidget(len(self.sequencer_items) - 1, new_item)
 
+    def generate_sequence_from_list_of_dicts(self, list_of_updated_settings: list):
+        # list_of_updated_settings = [
+        #     {
+        #         "downsample": {"scale": 16},
+        #         "regularization": {"enabled": True, "use_gpu": True},
+        #         "keep_on_gpu": True,
+        #     },
+        #     {
+        #         "downsample": {"scale": 8},
+        #         "regularization": {"enabled": True, "use_gpu": True},
+        #         "keep_on_gpu": True,
+        #     },
+        # ]
+        self.clear_all_sequences()
+        for settings_dict in list_of_updated_settings:
+            value_pairs = get_settings_from_dict(settings_dict)
+            for i, pair in enumerate(value_pairs):
+                is_last_entry = i == len(value_pairs) - 1
+                initial_state = (*pair, is_last_entry)
+                self.add_new_sequencer(initial_state)
+
     def remove_last_sequence(self):
         if len(self.sequencer_items) > 0:
             self.sequencer_items[-1].deleteLater()
             self.sequencer_items = self.sequencer_items[:-1]
 
-    def generate_options_sequence(self, options: OptionsClass) -> list[OptionsClass]:
-        options_sequence: list[OptionsClass] = []
+    def generate_options_sequence(self, options: BaseOptions) -> list[BaseOptions]:
+        options_sequence: list[BaseOptions] = []
         options_item = copy.deepcopy(options)
         for item in self.sequencer_items:
             if item.value() is None:
@@ -111,14 +137,22 @@ class SequencerWidget(QWidget):
             options_sequence += [options_item]
 
         return options_sequence
-    
+
     def clear_all_sequences(self):
         for i in range(len(self.sequencer_items)):
             self.remove_last_sequence()
 
-    def create_sequence_from_list(self):
-        self.clear_all_sequences()
-        # continue here; cant use the 
+
+def get_settings_from_dict(settings_dict: dict, name="", value_pairs=[]):
+    for setting_name, setting_value in settings_dict.items():
+        if isinstance(setting_value, dict):
+            value_pairs = get_settings_from_dict(
+                setting_value, f"{name}{setting_name}.", value_pairs
+            )
+        else:
+            value_pairs += [(f"{name}{setting_name}", setting_value)]
+    return value_pairs
+
 
 @switch_to_matplotlib_qt_backend
 def launch_pma_sequencer(
@@ -148,4 +182,5 @@ if __name__ == "__main__":
         int(screen_geometry.height() / 2),
     )
     window.show()
+    window.generate_sequence_from_list_of_dicts()
     sys.exit(app.exec_())
