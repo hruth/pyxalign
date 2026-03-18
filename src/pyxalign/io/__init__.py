c3fa043f6fca7cdfcea52580263e4f6ef194a581
Hanna Ruth <ruthanna@umich.edu>
2026-03-18 11:23:36 -0500

    API: added StandardData to pyxalign.io

--- src/pyxalign/io/__init__.py
+++ src/pyxalign/io/__init__.py
@@ -1,5 +1,11 @@
+from pyxalign.io.loaders.base import StandardData
 from . import loaders
 from .utils import load_options_from_h5_file
 from .save import save_loading_options_to_h5_file
 
-__all__ = ["loaders", "load_options_from_h5_file", "save_loading_options_to_h5_file"]
+__all__ = [
+    "loaders",
+    "load_options_from_h5_file",
+    "save_loading_options_to_h5_file",
+    "StandardData",
+]
